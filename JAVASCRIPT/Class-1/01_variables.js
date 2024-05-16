d14163d311d6231fc64ec7684f8e1f18674d20cb
Sudeshna-Mohanta <sudeshnamohanta20@gmail.com>
2024-05-16 16:33:54 +0530

    js file added

---
+++ JAVASCRIPT/Class-1/01_variables.js
@@ -0,0 +1,90 @@
+// declare a variable
+
+let num = 100;
+console.log(num); //to print the output
+
+let Num = 50;
+console.log(Num);
+
+// (space is not allowed while declaraing variables.)
+//let second num = 1000;
+
+let second_num = 1000; //snakeCase
+console.log(second_num);
+
+let secondNum = 1000; //camelCase
+console.log(secondNum);
+
+let decimal_num = 15.35;
+console.log(decimal_num);
+
+console.log(typeof decimal_num);
+
+//Task :
+//1 . Declare a variable and assign a value to it.
+
+let name = "Sudeshna";
+console.log(name);
+
+//2 . Declare different variables of numbers and assign different values to perform mathematical operations.
+
+// eg addition , subtraction , multiplication , division and modulus.
+
+// let num1 = 12;
+// let num2 = 13;
+// let addition = num1 + num2 ;
+// console.log(addition);
+
+// 1.
+
+let a = 10;
+let b = 20;
+let addition = a + b;
+console.log(addition);
+
+// 2.
+
+let c = 5;
+let d = 3;
+let subtraction = c - d;
+console.log(subtraction);
+
+// 3.
+
+let e = 2;
+let f = 4;
+let multiplication = e * f;
+console.log(multiplication);
+
+// 4.
+
+let g = 50;
+let h = 5;
+let division = g / h;
+console.log(division);
+
+// 5.
+
+let i = 11;
+let j = 2;
+let modulus = i % j;
+console.log(modulus);
+
+// 6.
+
+let k = 5;
+let l = 2;
+let exponent = k ** l;
+console.log(exponent);
+
+// 7.
+
+let m = 2;
+let increment = ++m;
+console.log(increment);
+
+// 8.
+
+let n = 10;
+let decrement = --n;
+console.log(decrement);
