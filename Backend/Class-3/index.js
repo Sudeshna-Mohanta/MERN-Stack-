018e7f2c2d095b48437bf14f162cc1f1df7329ca
Sudeshna-Mohanta <sudeshnamohanta20@gmail.com>
2024-06-08 13:04:32 +0530

    backend file added

--- Backend/Class-3/index.js
+++ Backend/Class-3/index.js
@@ -1,11 +1,24 @@
 // import modules here
 const express = require("express");
 const fs = require("fs"); //syntax
-require("dotenv");
+require("dotenv").config();
+const greeting = require("./data");
 
 //setup server
 const app = express();
 app.use(express.json());
+
+//middleware
+app.use((req, res, next) => {
+  greeting();
+  next();
+});
+
+//middlewares
+// app.use((req, res, next) => {
+//   console.log("Hello from Middleware");
+//   next();
+// });
 
 app.get("/", (req, res) => {
   res.json({ msg: "Hello from server " });
@@ -30,6 +43,12 @@ app.get("/append", (req, res) => {
   const sentence = "I am a student";
   fs.appendFileSync("logs.txt", sentence);
   res.json({ msg: "Successfully appended " });
+});
+
+app.post("/post", (req, res) => {
+  const user = req.body;
+  res.send(user);
+  fs.writeFileSync("./logs.txt", JSON.stringify(user));
 });
 
 //start the server
