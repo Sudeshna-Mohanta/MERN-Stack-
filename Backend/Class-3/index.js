fe49ca2eb57011faafe90e830313af79c793fe0e
Sudeshna-Mohanta <sudeshnamohanta20@gmail.com>
2024-06-11 23:35:51 +0530

    backend file added

--- Backend/Class-3/index.js
+++ Backend/Class-3/index.js
@@ -1,12 +1,21 @@
 // import modules here
 const express = require("express");
-const fs = require("fs"); //syntax
-require("dotenv").config();
+const fs = require("fs"); //syntax   //fs module provides a lot of functionality for interacting with the file system.
+require("dotenv").config(); //syntax  //the environment variables defined in the .env file are accessible throughout your Node.js app.
 const greeting = require("./data");
+
+const readData = () => {
+  const data = fs.readFileSync("data.json");
+  return JSON.parse(data);
+};
+
+const writeData = (data) => {
+  fs.writeFileSync("data.json", JSON.stringify(data, null, 2));
+};
 
 //setup server
 const app = express();
-app.use(express.json());
+app.use(express.json()); //express.json() -> built in middle ware function . used to set up the middleware that parses incoming requests with JSON payloads .  JSON - (Javascript Object Notation).
 
 //middleware
 app.use((req, res, next) => {
@@ -50,6 +59,72 @@ app.post("/post", (req, res) => {
   res.send(user);
   fs.writeFileSync("./logs.txt", JSON.stringify(user));
 });
+
+// get all users
+
+app.get("/users", (req, res) => {
+  const users = readData();
+  res.send(users);
+});
+
+// get users by id
+
+app.get("/users/:id", (req, res) => {
+  let { id } = req.params;
+  const users = readData();
+  const user = users.find((user, i) => {
+    return user.id === parseInt(id);
+  });
+
+  if (!user) {
+    res.send(`User not found!`);
+  } else {
+    res.send(user);
+  }
+});
+
+// edit the users info
+
+app.put("/users/:id", (req, res) => {
+  let { id } = req.params;
+  const users = readData();
+  const userid = users.find((user, i) => {
+    return user.id === parseInt(id);
+  });
+
+  if (!users) {
+    res.send(`User not found!`);
+  } else {
+    users[userid] = { id: users[userid], ...req.body };
+    writeData(users);
+    res.send(users[userid]);
+  }
+});
+
+// delete a user
+
+app.delete("/users/:id", (req, res) => {
+  let { id } = req.params;
+  const users = readData();
+  const userid = users.filter((user, i) => {
+    return user.id !== parseInt(id);
+  });
+
+  if (userid !== -1) {
+    // users[userid] = { id: users[userid], ...req.body };
+    writeData(users);
+    // res.send(users[userid]);
+    res.json({ msg: "Data deleted successfully" });
+  } else {
+    res.send(`User not found!`);
+  }
+});
+
+// app.get("/:id", (req, res) => {
+//   const user = req.body;
+//   res.send(user);
+//   fs.writeFileSync("./logs.txt", JSON.stringify(user));
+// });
 
 //start the server
 const port = process.env.port || 3000;
